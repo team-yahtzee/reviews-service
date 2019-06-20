2361f1302d76f549c90dae488e622d3ad3450e5f
kgxu <Kevinx88@gmail.com>
2019-06-20 16:49:52 -0400

    rewriting generator script

--- database-mysql/generator-main.js
+++ database-mysql/generator-main.js
@@ -85,12 +85,54 @@ var counter = 0;
 async.whilst(function() { return counter < times; }, function(next) {
   counter++;
   var reviewQuery = `INSERT INTO reviews (date, text, rating, user_id, apartment_id, has_response, owner_response) VALUES ?`;
-  var reviewItems = createReviewItems();
-  db.query(reviewQuery, [reviewItems], function(err, results) {
+  // var reviewItems = createReviewItems();
+  var inserts = [];
+  for (var i = 0; i < records; i++) {
+    if (Math.random() > 0.5) {
+      values = [
+        faker.date.month() +
+          " " +
+          faker.random.number({ min: 2015, max: 2019 }),
+        faker.lorem.sentences(Math.ceil(Math.random() * 6)),
+        Math.floor((() => Math.random() * 5)()) + 0.5,
+        faker.random.number({
+          min: 1,
+          max: 200
+        }),
+        faker.random.number({
+          min: 1,
+          max: 100
+        }),
+        Math.random() > 0.66,
+        faker.lorem.sentences(Math.ceil(Math.random() * 4))
+      ];
+      inserts.push(values);
+    } else {
+      values = [
+        faker.date.month() +
+          " " +
+          faker.random.number({ min: 2015, max: 2019 }),
+        faker.lorem.sentences(Math.ceil(Math.random() * 6)),
+        Math.floor((() => Math.random() * 5)()) + 1,
+        faker.random.number({
+          min: 1,
+          max: 200
+        }),
+        faker.random.number({
+          min: 1,
+          max: 100
+        }),
+        Math.random() > 0.66,
+        faker.lorem.sentences(Math.ceil(Math.random() * 4))
+      ];
+      inserts.push(values);
+    }
+  }
+  db.query(inserts, [reviewItems], function(err, results) {
     if (err) { return next(err); }
     console.log("Number of review records inserted: ", records);
     next();
-  });
+  })
   },
   function(err) {
     if (err) throw err;
