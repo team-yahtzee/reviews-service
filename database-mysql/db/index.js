e0e3f618bd2dca92721546a4af6ed8c482b24fef
kgxu <Kevinx88@gmail.com>
2019-06-20 19:45:44 -0400

    db config

--- database-mysql/db/index.js
+++ database-mysql/db/index.js
@@ -1,28 +1,28 @@
 var mysql = require('mysql');
 
-// var pool = mysql.createPool({
-//   host: 'localhost',
-//   connectionLimit: 10,
-//   user: 'root',
-//   password: 'new_password',
-//   database: 'reviews'
-// });
-
-var db = mysql.createConnection({
-  host: 'localhost',
+var pool = mysql.createPool({
+  host: '127.0.0.1',
+  connectionLimit: 10,
   user: 'root',
   password: 'new_password',
-  database: 'reviews',
-  port: 3306
+  database: 'reviews'
 });
 
-db.connect(function(err) {
-  if (err) {
-    console.error("error connection:", err.stack);
-    return;
-  }
-  console.log("Establish mysql connection");
-});
+// var db = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'new_password',
+//   database: 'reviews',
+//   port: 3306
+// });
+
+// db.connect(function(err) {
+//   if (err) {
+//     console.error("error connection:", err.stack);
+//     return;
+//   }
+//   console.log("Establish mysql connection");
+// });
 
 
-module.exports = db;
+module.exports = pool;
--- database-mysql/generator.js
+++ database-mysql/generator.js
@@ -1,228 +1,208 @@
 var mysql = require("mysql");
+var { promisify } = require("util");
 var path = require("path");
-var db = require("./db/index.js");
+var pool = require("./db/index.js");
 var faker = require("faker");
+var { apartmentAddresses } = require("./address.js");
 
-console.time();
-var maxRecordsSize = 2000000;
-var records = Math.floor(maxRecordsSize / 12);
+pool.getConnection(function(err, connection) {
+  console.time();
 
-var generateReviewValues = function() {
-  var inserts = [];
-  for (var i = 0; i < records; i++) {
-    if (Math.random() > 0.5) {
-      values = [
-        faker.date.month() +
-          " " +
-          faker.random.number({ min: 2015, max: 2019 }),
-        faker.lorem.sentences(Math.ceil(Math.random() * 6)),
-        Math.floor((() => Math.random() * 5)()) + 0.5,
-        faker.random.number({
-          min: 1,
-          max: 200
-        }),
-        faker.random.number({
-          min: 1,
-          max: 100
-        }),
-        Math.random() > 0.66,
-        faker.lorem.sentences(Math.ceil(Math.random() * 4))
-      ];
-      inserts.push(values);
-    } else {
-      values = [
-        faker.date.month() +
-          " " +
-          faker.random.number({ min: 2015, max: 2019 }),
-        faker.lorem.sentences(Math.ceil(Math.random() * 6)),
-        Math.floor((() => Math.random() * 5)()) + 1,
-        faker.random.number({
-          min: 1,
-          max: 200
-        }),
-        faker.random.number({
-          min: 1,
-          max: 100
-        }),
-        Math.random() > 0.66,
-        faker.lorem.sentences(Math.ceil(Math.random() * 4))
-      ];
-      inserts.push(values);
-    }
+  if (err) {
+    console.error("Database connection error: ", err.stack);
+  } else {
+    console.log("Connected to database.");
   }
-  return inserts;
-};
 
-var generateReviewData = function() {
-  var reviewQuery = `INSERT INTO reviews (date, text, rating, user_id, apartment_id, has_response, owner_response) VALUES ?`;
-  var reviewInserts = generateReviewValues();
-  db.query(reviewQuery, [reviewInserts], function(error, results) {
-    if (error) return console.error(error);
+  var maxRecordsSize = 2000000;
+  var times = 6;
+  var records = Math.floor(maxRecordsSize / (2 * times));
 
-    // users query
-    var userQuery = `INSERT INTO users (name, avatar) VALUES ?`;
-    var usersInserts = [];
+  // console.time("timer started");
+  var generateReviewValues = function() {
+    var inserts = [];
     for (var i = 0; i < records; i++) {
-      var values = [faker.name.firstName(), faker.internet.avatar()];
-      usersInserts.push(values);
+      if (Math.random() > 0.5) {
+        values = [
+          faker.date.month() +
+            " " +
+            faker.random.number({ min: 2015, max: 2019 }),
+          faker.lorem.sentences(Math.ceil(Math.random() * 6)),
+          Math.floor((() => Math.random() * 5)()) + 0.5,
+          faker.random.number({
+            min: 1,
+            max: 200
+          }),
+          faker.random.number({
+            min: 1,
+            max: 100
+          }),
+          Math.random() > 0.66,
+          faker.lorem.sentences(Math.ceil(Math.random() * 4))
+        ];
+        inserts.push(values);
+      } else {
+        values = [
+          faker.date.month() +
+            " " +
+            faker.random.number({ min: 2015, max: 2019 }),
+          faker.lorem.sentences(Math.ceil(Math.random() * 6)),
+          Math.floor((() => Math.random() * 5)()) + 1,
+          faker.random.number({
+            min: 1,
+            max: 200
+          }),
+          faker.random.number({
+            min: 1,
+            max: 100
+          }),
+          Math.random() > 0.66,
+          faker.lorem.sentences(Math.ceil(Math.random() * 4))
+        ];
+        inserts.push(values);
+      }
     }
-    db.query(userQuery, [usersInserts], function(err, results) {
-      if (err) return console.error(err.message);
+    return inserts;
+  };
 
-      // repeat once
-      var reviewQuery = `INSERT INTO reviews (date, text, rating, user_id, apartment_id, has_response, owner_response) VALUES ?`;
-      var reviewInserts = generateReviewValues();
-      db.query(reviewQuery, [reviewInserts], function(error, results) {
-        if (error) return console.error(error);
-  
-        // users query
-        var userQuery = `INSERT INTO users (name, avatar) VALUES ?`;
-        var usersInserts = [];
-        for (var i = 0; i < records; i++) {
-          var values = [faker.name.firstName(), faker.internet.avatar()];
-          usersInserts.push(values);
-        }
-        db.query(userQuery, [usersInserts], function(err, results) {
-          if (err) return console.error(err.message);
 
-          // repeat twice
-          var reviewQuery = `INSERT INTO reviews (date, text, rating, user_id, apartment_id, has_response, owner_response) VALUES ?`;
-          var reviewInserts = generateReviewValues();
-          db.query(reviewQuery, [reviewInserts], function(error, results) {
-            if (error) return console.error(error);
-      
-            // users query
-            var userQuery = `INSERT INTO users (name, avatar) VALUES ?`;
-            var usersInserts = [];
-            for (var i = 0; i < records; i++) {
-              var values = [faker.name.firstName(), faker.internet.avatar()];
-              usersInserts.push(values);
-            }
-            db.query(userQuery, [usersInserts], function(err, results) {
-              if (err) return console.error(err.message);
+  var generateReviewData = function() {
+    var reviewQuery = `INSERT INTO reviews (date, text, rating, user_id, apartment_id, has_response, owner_response) VALUES ?`;
+    var reviewInserts = generateReviewValues();
+    pool.query(reviewQuery, [reviewInserts], function(error, results) {
+      if (error) return console.error(error);
+      console.log("Rows inserted:", results.affectedRows);
 
-              // repeat third time
-              var reviewQuery = `INSERT INTO reviews (date, text, rating, user_id, apartment_id, has_response, owner_response) VALUES ?`;
-              var reviewInserts = generateReviewValues();
-              db.query(reviewQuery, [reviewInserts], function(error, results) {
-                if (error) return console.error(error);
+      // users query
+      var userQuery = `INSERT INTO users (name, avatar) VALUES ?`;
+      var usersInserts = [];
+      for (var i = 0; i < records; i++) {
+        var values = [faker.name.firstName(), faker.internet.avatar()];
+        usersInserts.push(values);
+      }
+      pool.query(userQuery, [usersInserts], function(err, results) {
+        if (err) return console.error(err.message);
+        console.log("Rows inserted: ", results.affectedRows);
 
-                // users query
-                var userQuery = `INSERT INTO users (name, avatar) VALUES ?`;
-                var usersInserts = [];
-                for (var i = 0; i < records; i++) {
-                  var values = [
-                    faker.name.firstName(),
-                    faker.internet.avatar()
-                  ];
-                  usersInserts.push(values);
-                }
-                db.query(userQuery, [usersInserts], function(err, results) {
-                  if (err) return console.error(err.message);
+        // repeat once
+        var reviewQuery = `INSERT INTO reviews (date, text, rating, user_id, apartment_id, has_response, owner_response) VALUES ?`;
+        var reviewInserts = generateReviewValues();
+        pool.query(reviewQuery, [reviewInserts], function(error, results) {
+          if (error) return console.error(error);
+          console.log("Rows inserted:", results.affectedRows);
 
-                  // repeat fourth time
-                  var reviewQuery = `INSERT INTO reviews (date, text, rating, user_id, apartment_id, has_response, owner_response) VALUES ?`;
-                  var reviewInserts = generateReviewValues();
-                  db.query(reviewQuery, [reviewInserts], function(error, results) {
-                    if (error) return console.error(error);
-              
+          // users query
+          var userQuery = `INSERT INTO users (name, avatar) VALUES ?`;
+          var usersInserts = [];
+          for (var i = 0; i < records; i++) {
+            var values = [faker.name.firstName(), faker.internet.avatar()];
+            usersInserts.push(values);
+          }
+          pool.query(userQuery, [usersInserts], function(err, results) {
+            if (err) return console.error(err.message);
+            console.log("Rows inserted: ", results.affectedRows);
 
-                    // users query
-                    var userQuery = `INSERT INTO users (name, avatar) VALUES ?`;
-                    var usersInserts = [];
-                    for (var i = 0; i < records; i++) {
-                      var values = [
-                        faker.name.firstName(),
-                        faker.internet.avatar()
-                      ];
-                      usersInserts.push(values);
-                    }
-                    db.query(userQuery, [usersInserts], function(err, results) {
-                      if (err) return console.error(err.message);
-                      console.log("Rows inserted: ", results.affectedRows);
+            // repeat twice
+            var reviewQuery = `INSERT INTO reviews (date, text, rating, user_id, apartment_id, has_response, owner_response) VALUES ?`;
+            var reviewInserts = generateReviewValues();
+            pool.query(reviewQuery, [reviewInserts], function(error, results) {
+              if (error) return console.error(error);
+              console.log("Rows inserted:", results.affectedRows);
 
-                      // repeat fourth time
-                      var reviewQuery = `INSERT INTO reviews (date, text, rating, user_id, apartment_id, has_response, owner_response) VALUES ?`;
-                      var reviewInserts = generateReviewValues();
-                      db.query(reviewQuery, [reviewInserts], function(error, results) {
-                        if (error) return console.error(error);
-                  
-                        // users query
-                        var userQuery = `INSERT INTO users (name, avatar) VALUES ?`;
-                        var usersInserts = [];
-                        for (var i = 0; i < records; i++) {
-                          var values = [
-                            faker.name.firstName(),
-                            faker.internet.avatar()
-                          ];
-                          usersInserts.push(values);
-                        }
-                        db.query(userQuery, [usersInserts], function(err, results) {
-                          if (err) return console.error(err.message);
-                          db.end();
-                          console.timeEnd();
+              // users query
+              var userQuery = `INSERT INTO users (name, avatar) VALUES ?`;
+              var usersInserts = [];
+              for (var i = 0; i < records; i++) {
+                var values = [faker.name.firstName(), faker.internet.avatar()];
+                usersInserts.push(values);
+              }
+              pool.query(userQuery, [usersInserts], function(err, results) {
+                if (err) return console.error(err.message);
+                console.log("Rows inserted: ", results.affectedRows);
+
+                // repeat third time
+                var reviewQuery = `INSERT INTO reviews (date, text, rating, user_id, apartment_id, has_response, owner_response) VALUES ?`;
+                var reviewInserts = generateReviewValues();
+                pool.query(reviewQuery, [reviewInserts], function(error, results) {
+                  if (error) return console.error(error);
+                  console.log("Rows inserted:", results.affectedRows);
+
+                  // users query
+                  var userQuery = `INSERT INTO users (name, avatar) VALUES ?`;
+                  var usersInserts = [];
+                  for (var i = 0; i < records; i++) {
+                    var values = [
+                      faker.name.firstName(),
+                      faker.internet.avatar()
+                    ];
+                    usersInserts.push(values);
+                  }
+                  pool.query(userQuery, [usersInserts], function(err, results) {
+                    if (err) return console.error(err.message);
+                    console.log("Rows inserted: ", results.affectedRows);
+
+                    // repeat fourth time
+                    var reviewQuery = `INSERT INTO reviews (date, text, rating, user_id, apartment_id, has_response, owner_response) VALUES ?`;
+                    var reviewInserts = generateReviewValues();
+                    pool.query(reviewQuery, [reviewInserts], function(error, results) {
+                      if (error) return console.error(error);
+                      console.log("Rows inserted:", results.affectedRows);
+
+                      // users query
+                      var userQuery = `INSERT INTO users (name, avatar) VALUES ?`;
+                      var usersInserts = [];
+                      for (var i = 0; i < records; i++) {
+                        var values = [
+                          faker.name.firstName(),
+                          faker.internet.avatar()
+                        ];
+                        usersInserts.push(values);
+                      }
+                      pool.query(userQuery, [usersInserts], function(err, results) {
+                        if (err) return console.error(err.message);
+                        console.log("Rows inserted: ", results.affectedRows);
+
+                        // repeat fourth time
+                        var reviewQuery = `INSERT INTO reviews (date, text, rating, user_id, apartment_id, has_response, owner_response) VALUES ?`;
+                        var reviewInserts = generateReviewValues();
+                        pool.query(reviewQuery, [reviewInserts], function(error, results) {
+                          if (error) return console.error(error);
+                          console.log("Rows inserted:", results.affectedRows);
+
+                          // users query
+                          var userQuery = `INSERT INTO users (name, avatar) VALUES ?`;
+                          var usersInserts = [];
+                          for (var i = 0; i < records; i++) {
+                            var values = [
+                              faker.name.firstName(),
+                              faker.internet.avatar()
+                            ];
+                            usersInserts.push(values);
+                          }
+                          pool.query(userQuery, [usersInserts], function(err, results) {
+                            if (err) return console.error(err.message);
+                            console.log("Rows inserted: ", results.affectedRows);
+                            // connection.release();
+                            console.timeEnd();
+                          });
                         });
+                        //end repeat
                       });
-                      //end repeat
                     });
+                    //end repeat
                   });
-                  //end repeat
                 });
+                //end repeat
               });
-              //end repeat
             });
+            //end repeat
           });
-          //end repeat
         });
+        //end repeat
       });
-      //end repeat
     });
-  });
-};
+    //end
+  };
 
-generateReviewData();
-
-// while (times > 0) {
-//   var reviewQuery = `INSERT INTO reviews (date, text, rating, user_id, apartment_id, has_response, owner_response) VALUES ?`
-//   var reviewInserts = generateReviewValues();
-//   db.query(reviewQuery, [reviewInserts], function(error, results) {
-//   if (error) return console.error(error);
-//
-
-//     var apartmentQuery = `INSERT INTO apartments (address, owned_by_user_id) VALUES ?`
-//     var apartmentInserts = [];
-//     for (var i = 0; i < records; i++) {
-//       var values = [apartmentAddresses[i], i + 1];
-//       apartmentInserts.push(values);
-//     }
-//     db.query(apartmentQuery, [apartmentInserts], function(error, results) {
-//       if (error) return console.error(error.message);
-//    
-
-//       var userQuery = `INSERT INTO users (name, avatar) VALUES ?`;
-//       var usersInserts = [];
-//       for (var i = 0; i < records; i++) {
-//         var values = [faker.name.firstName(), faker.internet.avatar()];
-//         usersInserts.push(values);
-//       }
-//       db.query(userQuery, [usersInserts], function(err, results) {
-//         if (err) return console.error(err.message);
-//         console.log("Rows inserted: ", results.affectedRows);
-//       });
-//     });
-//   });
-//   times -= 1;
-// }
-// connection.release();
-
-// db.end(function(err) {
-//   if (err) {
-//     return console.error(err.message);
-//   } else {
-//     console.log("Closed all connections");
-//   }
-// });
-
-// process.on('exit', function() {
-//   console.timeEnd(generatorsCombined);
-// });
+  generateReviewData();
+});
